5e380bd6a55872a4303dd11ea6813d85d90999ef
“Vimal <vikashkaushik232@gmail.com>
2022-04-10 06:36:42 +0530

    added some comments

--- java_code/Java_Interface/FirstInterface.java
+++ java_code/Java_Interface/FirstInterface.java
@@ -1,19 +1,22 @@
 interface FirstInterface {
-    public void myMethod(); // interface method
+    public void myMethod(); // interface method (does not have a body)
 
 }
 
 interface SecondInterface {
-    public void myOtherMethod(); // interface method
+    public void myOtherMethod(); // interface method (does not have a body)
 
 }
 
+// DemoClass "implements" the FirstInterface & SecondInterface
 class DemoClass implements FirstInterface, SecondInterface {
     public void myMethod() {
+        // The body of myMethod() is provided here
         System.out.println("Some text..");
     }
 
     public void myOtherMethod() {
+        // The body of myOtherMethod() is provided here
         System.out.println("Some other text...");
     }
 }
