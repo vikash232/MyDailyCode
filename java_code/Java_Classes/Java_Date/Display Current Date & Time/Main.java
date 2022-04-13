db5507eadcadf28e282115f69a93e7dceb963d17
“Vimal <vikashkaushik232@gmail.com>
2022-04-13 20:38:26 +0530

    rearraged the directory order

---
+++ java_code/Java_Classes/Java_Date/Display Current Date & Time/Main.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime; // import the LocalDataTime class
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+    }
+}
---
+++ java_code/Java_Classes/Java_Date/Display Current Date/Main.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate; // import the LocalDate class
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate myObj = LocalDate.now(); // Create a date object
+        System.out.print(myObj); // Display the current date
+    }
+}
