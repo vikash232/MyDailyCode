bac0742b803d9ca36498331b85ab96729c3608e9
“Vimal <vikashkaushik232@gmail.com>
2022-04-17 08:28:54 +0530

    added file handling codes

---
+++ java_code/Java_File_Handling/Java_Files/File.java
@@ -0,0 +1,7 @@
+import java.io.File; // Import the File class
+
+public class File {
+    public static void main(String[] args) {
+        File myObj = new File("filename.txt"); // Specify the filename
+    }
+}
