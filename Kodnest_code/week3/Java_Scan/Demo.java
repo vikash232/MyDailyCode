b5040689d3dd1bbb5a354cd94d336957007fcf02
“Vimal <vikashkaushik232@gmail.com>
2022-04-26 05:55:10 +0530

    Entered scanned code

--- Kodnest_code/week3/Java_Scan/Demo.java
+++ Kodnest_code/week3/Java_Scan/Demo.java
@@ -4,9 +4,13 @@ class Demo {
     public static void main(String args[]) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a Number:-");
+        System.out.println("Enter a number");
         int a = scan.nextInt();
-        System.out.println("Entered number is: " + a);
+        System.out.println("Enter number is:" + a);
+        System.out.println("_______________________________");
+        System.out.println("Enter you height");
+        float b = scan.nextFloat();
+        System.out.println("Your height is:" + b);
     }
 
 }
