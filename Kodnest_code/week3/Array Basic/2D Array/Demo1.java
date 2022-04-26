1369f0cdcbe054ea6306da977173d40bfc47591a
“Vimal <vikashkaushik232@gmail.com>
2022-04-26 11:09:34 +0530

    Update 2Ds Jagged Java Code

---
+++ Kodnest_code/week3/Array Basic/2D Array/Demo1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int arr[][] = new int[3][4];
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr[i].length - 1; j++) {
+                System.out.println("Enter the Collegs" + " " + i + "and Students" + " " + j);
+                arr[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println("The Arrays content are...");
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr[i].length - 1; j++) {
+                System.out.println(arr[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
