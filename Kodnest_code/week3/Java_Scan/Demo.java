9573d911c43f5f96fab66deff314311ac2956ea4
“Vimal <vikashkaushik232@gmail.com>
2022-04-26 06:07:18 +0530

    Entered some extra code for Scan keyword

--- Kodnest_code/week3/Java_Scan/Demo.java
+++ Kodnest_code/week3/Java_Scan/Demo.java
@@ -11,6 +11,16 @@ class Demo {
         System.out.println("Enter you height");
         float b = scan.nextFloat();
         System.out.println("Your height is:" + b);
+        System.out.println("_______________________________");
+        System.out.println("Are you married? Enter True or False");
+        boolean c = scan.nextBoolean();
+        System.out.println("Your maritial status is: " + c);
+        System.out.println("_______________________________");
+        System.out.println("Enter your Father name");
+        String d = scan.next();
+        String e = scan.next();
+        System.out.println("Your Father name is: " + d + " " + e);
+
     }
 
 }
