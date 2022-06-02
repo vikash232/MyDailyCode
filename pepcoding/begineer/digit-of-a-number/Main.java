0dc35cf11a089712b0d7fbfe78a0961b487c46ca
“Vimal <vikashkaushik232@gmail.com>
2022-06-02 23:50:36 +0530

    add digit of a number code

---
+++ pepcoding/begineer/digit-of-a-number/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/* Problem statement:- take input like 65784383
+ print output like-
+ 6
+ 5
+ 7
+ 8
+ 4
+ 3
+ 8
+ 3
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int nod = 0;
+        int temp = n;
+        while (temp != 0) {
+            temp = temp / 10;
+            nod++;
+        }
+
+        int div = (int) Math.pow(10, nod - 1);
+        while (div != 0) {
+            int q = n / div;
+            System.out.println(q);
+
+            n = n % div;
+            div = div / 10;
+        }
+    }
+
+}
