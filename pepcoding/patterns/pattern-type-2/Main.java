7d42a15ea52a2332123e5212bac27b8204be580c
“Vimal <johndoe@example.com>
2022-06-23 22:38:34 +0530

    patterntype2

---
+++ pepcoding/patterns/pattern-type-2/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write your code here
+        int n = scn.nextInt();
+        for (int i = n; i >= 1; i++) {
+            for (int j = 1; j <= 1; j++) {
+                System.out.println("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
