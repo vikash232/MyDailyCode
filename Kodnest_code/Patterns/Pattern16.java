363ea2965f44a86e2c1e2635dd12399dfdef5579
“Vimal <vikashkaushik232@gmail.com>
2022-07-09 23:56:16 +0530

    added 16th pattern

--- Kodnest_code/Patterns/Pattern16.java
+++ Kodnest_code/Patterns/Pattern16.java
@@ -1,8 +1,8 @@
 /*
 _$$$$$
-__$$$$$
-___$$$$$
-____$$$$$
+__$$$$$     Note:- ith = no. of lines
+___$$$$$            jth = no. of space
+____$$$$$           kth = no. of $"s
 _____$$$$$
 ____$$$$$
 ___$$$$$
@@ -11,5 +11,24 @@ _$$$$$
  */
 
 public class Pattern16 {
-
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 5; k++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 5; k++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
 }
