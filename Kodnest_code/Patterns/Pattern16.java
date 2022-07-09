d6e480e168ebebf3d4b32e1c29a77ecefe5df2a7
“Vimal <vikashkaushik232@gmail.com>
2022-07-09 23:59:44 +0530

    done 16th

--- Kodnest_code/Patterns/Pattern16.java
+++ Kodnest_code/Patterns/Pattern16.java
@@ -13,7 +13,7 @@ _$$$$$
 public class Pattern16 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 4; j >= i; j--) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= 5; k++) {
