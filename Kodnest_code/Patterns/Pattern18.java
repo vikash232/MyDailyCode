669991a9baf5bf6d00b754c9e219f3a0ea45a482
“Vimal <vikashkaushik232@gmail.com>
2022-07-10 21:57:49 +0530

    added Pattern18

--- Kodnest_code/Patterns/Pattern18.java
+++ Kodnest_code/Patterns/Pattern18.java
@@ -1,10 +1,20 @@
 /*
- _____$$$$$
- ____$$$$
- ___$$$
+ _____$$$$$  Note: ith = no. of lines
+ ____$$$$           jth = no. of spaces
+ ___$$$             kth = no. of $'s
  __$$
  _$
  */
 public class Pattern18 {
-
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 5; k >= i; k--) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
 }
