14b7956eccf3a381e963312886cc5e66494280bb
“Vimal <vikashkaushik232@gmail.com>
2022-07-11 12:51:10 +0530

    added Pattern 21

--- Kodnest_code/Patterns/Pattern21.java
+++ Kodnest_code/Patterns/Pattern21.java
@@ -1,12 +1,23 @@
 /*
- _____$
- ____$$$
- ___$$$$$
+ _____$         Note:- ith = no. of lines
+ ____$$$                jth = no. of spaces
+ ___$$$$$               kth = no. of $'s
  __$$$$$$$
  _$$$$$$$$$
 
  */
 
 public class Pattern21 {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
 
 }
