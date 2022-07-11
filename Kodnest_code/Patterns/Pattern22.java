614f81a75b2cf4ffcb2595164a496ee15a2e2be7
“Vimal <vikashkaushik232@gmail.com>
2022-07-11 12:56:08 +0530

    added another patter22

--- Kodnest_code/Patterns/Pattern22.java
+++ Kodnest_code/Patterns/Pattern22.java
@@ -1,11 +1,21 @@
 /*
- _$$$$$$$$
- __$$$$$$$
- ___$$$$$
- ____$$$
+ _$$$$$$$$$
+ __$$$$$$$  Note:- ith = no. of lines
+ ___$$$$$           jth = no. of spaces 
+ ____$$$            kth = no. of $'s
  _____$
  */
 
 public class Pattern22 {
-
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {// ith = no. of lines
+            for (int j = 1; j <= i; j++) {// jth = no. of spaces
+                System.out.print(" ");
+            }
+            for (int k = 9; k >= 2 * i - 1; k--) {// kth = no. of $'s
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
 }
