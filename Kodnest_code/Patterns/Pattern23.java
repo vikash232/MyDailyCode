59c402534a46c9f669f922a0cc680e5c55da4df7
“Vimal <vikashkaushik232@gmail.com>
2022-07-11 13:18:03 +0530

    added Pattern23

--- Kodnest_code/Patterns/Pattern23.java
+++ Kodnest_code/Patterns/Pattern23.java
@@ -1,9 +1,9 @@
 /*
  _____$
  ____$$$
- ___$$$$$
- __$$$$$$$
- _$$$$$$$$$
+ ___$$$$$       Note:- ith = no. of lines
+ __$$$$$$$              jth = no. of spaces
+ _$$$$$$$$$             kth = no. of $'s
  _$$$$$$$$$
  __$$$$$$$
  ___$$$$$
@@ -12,5 +12,24 @@
  */
 
 public class Pattern23 {
-
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) { // ith = no. of lines
+            for (int j = 5; j >= i; j--) { // jth = no. of spaces
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) { // kth = no. of $'s
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 9; k >= 2 * i - 1; k--) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
 }
