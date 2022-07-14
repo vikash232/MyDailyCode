c1c60bece0aac42b1e70be8b777cb80d14985621
“Vimal <vikashkaushik232@gmail.com>
2022-07-14 17:36:56 +0530

    ended with 2D jagged arrays

--- Kodnest_code/week3/july_array/array2D/Jagged2.java
+++ Kodnest_code/week3/july_array/array2D/Jagged2.java
@@ -3,7 +3,7 @@ Problem Statement
 
 Banks       Customers
 0           0 1 2 3 4
-1           0 1 2 
+1           0 1 2       no. of columns = ith(5)
 2           0 1 2
 3           0 1 2 3
 4           0 1
@@ -12,5 +12,28 @@ Banks       Customers
 import java.util.*;
 
 public class Jagged2 {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int arr[][] = new int[5][];
+        arr[0] = new int[5];
+        arr[1] = new int[3];
+        arr[2] = new int[3];
+        arr[3] = new int[4];
+        arr[4] = new int[2];
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr[i].length - 1; j++) {
+                System.out.println("Enter the Bank " + i + " and Enter the Customers " + j);
+                arr[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println("Arrays contents are...");
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr[i].length - 1; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 
 }
