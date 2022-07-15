32869e05d60e14ecf11460611672828489cbad14
“Vimal <vikashkaushik232@gmail.com>
2022-07-15 22:23:09 +0530

    remove code redundancy

---
+++ Kodnest_code/week3/july_array/remove_code_redundancy_from_array/2D_array_redundancy/Jagged1.java
@@ -0,0 +1,14 @@
+/*
+Remove Code Redundany from this 2d arrays
+
+ Colleges   Students
+ 0          0 1 2
+ 1          0 1 2
+ 2          0 1 2
+ 3          0 1 2
+ 4          0 1 2
+ */
+
+ import java.util.Scanner;
+
+ pu
