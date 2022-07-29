5b2e768b09e3a4bd2720aa01cdf89e088e788ff3
[Vikash Kaushik] <vikashkaushik232@gmail.com>
2022-07-29 20:33:17 +0530

    added k8s code

---
+++ CWH_JAVA/src/com/company/CWH_05_TakingInput.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class CWH_05_TakingInput {
+
+}
--- CWH_JAVA/src/com/company/Main.java
+++ CWH_JAVA/src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.print("The sum of these numbers is: ");
+        int num1 = 6;
+        int num2 = 4;
+        int num3 = 7;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
     }
 }
+
+
+// add the number
+// AddTwoNumber --> pascal naming convention
+// addTwoNumber --> camel case naming convention
