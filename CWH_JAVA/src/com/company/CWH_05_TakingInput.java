1c59c6327a4e2d3d0b5cd61aa08e6188cd771cb3
[Vikash Kaushik] <vikashkaushik232@gmail.com>
2022-08-04 23:18:28 +0530

    added code

--- CWH_JAVA/src/com/company/CWH_05_TakingInput.java
+++ CWH_JAVA/src/com/company/CWH_05_TakingInput.java
@@ -1,5 +1,19 @@
 package com.company;
+import java.util.Scanner;
 
 public class CWH_05_TakingInput {
+    public static void main(String[] args){
+        System.out.println("It works!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of these number is");
+        System.out.println(sum);
+
+    }
+
 
 }
