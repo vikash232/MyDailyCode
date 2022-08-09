5f61fc396a360a746f7a926bf109ec0e8e8b6a4a
[Vikash Kaushik] <vikashkaushik232@gmail.com>
2022-08-09 08:08:45 +0530

    done some changes

---
+++ CWH_JAVA/src/com/company/cwh_13_strings.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.*;
+
+public class cwh_13_strings {
+    public static void main(String[] args){
+//        String name = new String("Harry");
+//        System.out.println(name);
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
