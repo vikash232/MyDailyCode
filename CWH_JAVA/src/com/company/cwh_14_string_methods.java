260be4442a21e47948522b457dca3d559ba49a95
[Vikash Kaushik] <vikashkaushik232@gmail.com>
2022-08-13 18:57:07 +0530

    done some changes

--- CWH_JAVA/src/com/company/cwh_14_string_methods.java
+++ CWH_JAVA/src/com/company/cwh_14_string_methods.java
@@ -2,17 +2,23 @@ package com.company;
 
 public class cwh_14_string_methods {
     public static void main(String[] args){
-        String name = "harry";
-        String a1 = new String("Harry");
-        System.out.println( a1);
+//        String name = "harry";
+//        String a1 = new String("Harry");
+//        System.out.println( a1);
 
-        //for all lowercase
-        String lstring = name.toLowerCase();
-        System.out.println(lstring);
+//        //for all lowercase
+//        String lstring = name.toLowerCase();
+//        System.out.println(lstring);
+//
+//        //for all lowercase
+//        String ustring = name.toUpperCase();
+//        System.out.println(ustring);
 
-        //for all lowercase
-        String ustring = name.toUpperCase();
-        System.out.println(ustring);
+//        String nonTrimmedString = " Harry ";
+//        System.out.println(nonTrimmedString);
+//
+//        String trimmedString = nonTrimmedString.trim();
+//        System.out.println(trimmedString);
 
     }
 }
