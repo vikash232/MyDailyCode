0216e0f8330a4aa44ccad90e826472347dde82c6
[Vikash Kaushik] <vikashkaushik232@gmail.com>
2022-08-13 18:41:10 +0530

    done some changes

---
+++ CWH_JAVA/src/com/company/cwh_14_string_methods.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class cwh_14_string_methods {
+    public static void main(String[] args){
+        String a = "harry";
+        String a1 = new String("Harry");
+        System.out.println(a + " " + a1);
+    }
+}
