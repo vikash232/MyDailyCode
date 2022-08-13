04509f5fdef010206ece3fa44a9c40aa96740e25
[Vikash Kaushik] <vikashkaushik232@gmail.com>
2022-08-13 18:50:01 +0530

    done some changes

--- CWH_JAVA/src/com/company/cwh_14_string_methods.java
+++ CWH_JAVA/src/com/company/cwh_14_string_methods.java
@@ -2,8 +2,17 @@ package com.company;
 
 public class cwh_14_string_methods {
     public static void main(String[] args){
-        String a = "harry";
+        String name = "harry";
         String a1 = new String("Harry");
-        System.out.println(a + " " + a1);
+        System.out.println( a1);
+
+        //for all lowercase
+        String lstring = name.toLowerCase();
+        System.out.println(lstring);
+
+        //for all lowercase
+        String ustring = name.toUpperCase();
+        System.out.println(ustring);
+
     }
 }
