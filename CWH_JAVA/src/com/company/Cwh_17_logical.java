aa7d55d10b6f23ea1a59c8070749ba50adfd1ec9
[Vikash Kaushik] <vikashkaushik232@gmail.com>
2022-08-18 19:23:59 +0530

    updated code

---
+++ CWH_JAVA/src/com/company/Cwh_17_logical.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class cwh_17_logical {
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+        if (a && b){
+            System.out.println("Y");
+        }
+        else{
+            System.out.println("N");
+        }
+    }
+}
